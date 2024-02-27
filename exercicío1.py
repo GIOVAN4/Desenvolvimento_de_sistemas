4de5d46bcf632244927e9a7fb3e08b28e842cb2b
GIOVAN4 <125381987+GIOVAN4@users.noreply.github.com>
2024-02-27 13:56:33 -0300

    Create exercicío1.py

---
+++ exercicío1.py
@@ -0,0 +1,16 @@
+# Como se chama um variavel?
+
+idade = 16
+nome = "Giovana"
+preco = 15.00
+
+# Como podemos modar essas variaveis no programa?
+# Função print()
+
+print("A idade é: ," idade)
+
+# Como podemos mostrar variaveis dentro do strng?
+
+text = "Vou comprar {} que custa {} real"
+
+print(text,format{"Banana", preco})
